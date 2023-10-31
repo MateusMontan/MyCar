7726312339833ac994f823a5cfc00f70992a3149
Mateus Montan <mateusinfocefetmg@gmail.com>
2023-10-31 20:47:39 -0300

    update

--- app/src/main/java/com/example/mycar/tela/Usuario/EditarUsuario.java
+++ app/src/main/java/com/example/mycar/tela/Usuario/EditarUsuario.java
@@ -19,8 +19,6 @@ import com.google.firebase.database.FirebaseDatabase;
 import java.util.HashMap;
 import java.util.Map;
 
-import button.OnClickListener;
-
 public class EditarUsuario extends AppCompatActivity {
 
     private TextView textView;
